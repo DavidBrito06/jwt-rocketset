4d797b398f6a53611ab1f0d06d3ab9d2b0186c7b
DavidBrito06 <davidbritodejesus1236@gmail.com>
2025-08-20 10:01:23 -0300

    Implement login functionality with user verification and JWT token creation

---
+++ src/controllers/login_creator.py
@@ -0,0 +1,43 @@
+from src.models.interface.user_repository import UserRepositoryInterface
+from src.drivers.password_haandler import PasswordHandler
+from src.drivers.jwt_handler import JwtHandler
+
+class LoginCreator:
+    def __init__(self, user_repository: UserRepositoryInterface)-> None:
+        self.__user_repository = user_repository
+        self.__password_Handler = PasswordHandler
+        self.__jwt_handler = JwtHandler
+    
+    def create(self, username: str, password: str) -> dict:
+        user = self.__find_user(username)
+        user_id = user[0]
+        hashed_password = user[2]
+
+        self.__verify_correct_password(password, hashed_password)
+        token = self.__create_jtw_token(user_id)
+        return self.__format_response(username, token)
+
+    def __find_user(self, username: str) -> tuple[int, str, str]:
+        user = self.__user_repository.get_user_by_username(username)
+        if not user: raise Exception("User not found")
+
+        return user
+    
+    def __verify_correct_password(self, password: str, hashed_password: str) -> None:
+        is_passowrd_correct = self.__password_Handler.check_password(password, hashed_password)
+
+        if not is_passowrd_correct: raise Exception("wrong password")
+
+    def __create_jtw_token(self,user_id: int) -> str:
+        payload = {"user_id": user_id}
+        token = self.__jwt_handler.create_jwt_token(payload)
+
+        return token
+    
+    def __format_response(self, username: str, token: str) -> dict:
+        return {
+            "access": True,
+            "username": username,
+            "token": token
+        }
+        
